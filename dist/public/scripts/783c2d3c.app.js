de2c88e22b67fa2ed5b06fea6a7ea403d9c4f51a
Peter Stephens <peter.m.stephens@gmail.com>
2013-07-06 14:51:59 +0800

    Added reved files

---
+++ dist/public/scripts/783c2d3c.app.js
@@ -0,0 +1,62 @@
+'use strict';
+angular.module('panicApp', [
+  'panicApp.Controllers',
+  'panicApp.referenceDataServices',
+  'panicApp.designBuildDirectives',
+  'panicApp.designBuildFilters',
+  'ui.bootstrap'
+]).config([
+  '$routeProvider',
+  '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'StaticPageCtrl'
+    }).when('/fit', {
+      templateUrl: 'views/fit.html',
+      controller: 'StaticPageCtrl'
+    }).when('/fabrics', {
+      templateUrl: 'views/fabrics.html',
+      controller: 'StaticPageCtrl'
+    }).when('/party', {
+      templateUrl: 'views/party.html',
+      controller: 'StaticPageCtrl'
+    }).when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'StaticPageCtrl'
+    }).when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'StaticPageCtrl'
+    }).when('/collection', {
+      templateUrl: 'views/dressCollection.html',
+      controller: 'DressCollectionCtrl'
+    }).when('/design', {
+      templateUrl: 'views/silhouettes.html',
+      controller: 'SilhouetteCtrl'
+    }).when('/olddesign/:rangeId', {
+      templateUrl: 'views/designBuildDirective.html',
+      controller: 'DesignBuildCtrl'
+    }).when('/olddesign/:rangeId/:itemId', {
+      templateUrl: 'views/designBuildDirective.html',
+      controller: 'DesignBuildCtrl'
+    }).when('/design/:rangeId', {
+      templateUrl: 'views/newDesignBuild.html',
+      controller: 'NewDesignBuildCtrl'
+    }).when('/design/:rangeId/:itemId', {
+      templateUrl: 'views/newDesignBuild.html',
+      controller: 'NewDesignBuildCtrl'
+    }).when('/designAdmin/:rangeId', {
+      templateUrl: 'views/designBuildAdmin.html',
+      controller: 'NewDesignBuildCtrl'
+    }).when('/purchase', {
+      templateUrl: 'views/sizing.html',
+      controller: 'StaticPageCtrl'
+    }).when('/adminCollection', {
+      templateUrl: 'views/adminCollection.html',
+      controller: 'CollectionCtrl'
+    }).when('/adminCollection/:rangeId', {
+      templateUrl: 'views/adminRangeDetails.html',
+      controller: 'RangeDetailsCtrl'
+    }).otherwise({ redirectTo: '/' });
+  }
+]);
---
+++ gruntfile/Gruntfile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= props.license %> */\n',
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['lib/<%%= pkg.name %>.js'],
+                dest: 'dist/<%%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {}
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.libTest.src %>',
+                tasks: ['jshint:libTest', 'qunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
