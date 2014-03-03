14ea352d201fe58f26e3eaa1b38d60545b27461d
Peter Stephens <peter.m.stephens@gmail.com>
2014-03-03 23:29:51 +0800

    Collection item pics

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,8 +224,9 @@ module.exports = function (grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg}',
+          cwd: '<%= yeoman.app %>/images/**',
+//          src: '{,*/}*.{png,jpg,jpeg}',
+          src: '*.{png,jpg,jpeg}',
           dest: '<%= yeoman.public_dist %>/images'
         }]
       }
